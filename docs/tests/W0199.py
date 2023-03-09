427abc9f7df6bbf8e1096e3b92341ebf28f6370f
David Marinho <david.marinho@codacy.com>
2023-03-09 15:16:19 +0000

    bump pylint to 2.17.0

--- docs/tests/W0199.py
+++ docs/tests/W0199.py
@@ -1,3 +1,15 @@
-##Patterns: W0199
+#Patterns: W0199
 ##Warn: W0199
-assert (1, None)
+assert (1 == 1, 2 == 2), "no error"
+##Warn: W0199
+assert (1 == 1, 2 == 2)
+##Warn: W0199
+assert 1 == 1, "no error"
+##Warn: W0199
+assert (1 == 1,), "no error"
+##Warn: W0199
+assert (1 == 1,)
+##Warn: W0199
+assert (1 == 1, 2 == 2, 3 == 5), "no error"
+##Warn: W0199
+assert (True, 'error msg')
