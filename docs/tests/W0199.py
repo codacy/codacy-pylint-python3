2b1823c8da6dbcdc57765c3fcaf6f48a4564aa94
David Marinho <david.marinho@codacy.com>
2023-03-09 15:47:29 +0000

    fix test W0199

--- docs/tests/W0199.py
+++ docs/tests/W0199.py
@@ -4,7 +4,7 @@ assert (1 == 1, 2 == 2), "no error"
 ##Warn: W0199
 assert (1 == 1, 2 == 2)
 ##Warn: W0199
-assert 1 == 1, "no error"
+assert (1 == 1), "no error"
 ##Warn: W0199
 assert (1 == 1,), "no error"
 ##Warn: W0199
